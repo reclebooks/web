6e8687ce8472464a7aefc2a4aaa98853cc660797
Noriaki77 <sepiano15@gmail.com>
2022-06-05 22:11:30 +0900

    [FEAT] 데이터 작업 진행중

---
+++ index.js
@@ -0,0 +1,47 @@
+
+const generateBookElement = (usedBookDto)=>{
+    const {id, usedBookCost, image} = usedBookDto;
+    const {cost, name, colleges} = usedBookDto.book;
+    return `<div class="bookElement">
+    <a href="${serverUrl}/used/${id}">
+    <section>
+        <article
+            class="book-kind card"
+            style="
+                background-image: url('${image[0]}');
+            "
+        >
+            <section class="book-kind blur">
+                <section class="description-box">
+                    <article class="lecture">기초컴퓨터프로그래밍</article>
+                    <article class="book-name">
+                        ${name}
+                    </article>
+                    <article class="origin-cost">${cost}원</article>
+                    <article class="used-cost">${usedBookCost}원</article>
+                </section>
+            </section>
+    
+            <section class="category">
+                <ul class="college-category">
+                    <li class="college">${colleges[0]}</li>
+                    <li class="major-college">${colleges[1]}</li>
+                    <li class="major-category">${colleges[2]}</li>
+                </ul>
+            </section>
+        </article>
+    </section>
+    </a>
+    </div>`
+
+}
+
+const search = async ()=>{
+    const books = await fetch(`${serverUrl}/used${location.search}`);
+    return books;
+}
+
+const loadUsedBooks = async() => {
+    const books = await search();
+    books.forEach(book=>document.querySelector('.bookGrid').)
+}
